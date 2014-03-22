b98166a2f18e8183a1e0bc8088aae9a88ace5a68
Nuno Valente <nuno.av@live.com.pt>
2014-03-22 22:25:19 +0000

    Working on map generation
    Plain map generation concluded

--- maps_create.c
+++ maps_create.c
@@ -18,30 +18,44 @@ Changes done: Preparing code for terrain generation, starting with plain terrain
 #define MAX_Y 100
 #define MIN_X 0
 #define MIN_Y 0
+#define MAX_TERRAIN_HEIGHT_PLAINS_MAP 80
 
 typedef struct
 {
     int x, y;
 } tCoordinates;
 
-void createMap(int map[MAX_X][MAX_Y]);
+void createMap(int mapLayout[MAX_X][MAX_Y]);
 int timeSeconds();
-
 
 int main(void)
 {
-    int map[MAX_X][MAX_Y]; /*Matrix containing the map information*/
+    int mapLayout[MAX_X][MAX_Y] = {{0}}; /*Matrix containing the map information initialized with value 0 everywhere*/
+    /*Since the array is initialized with the value 0, which stands for empty in the map, we won't have to fill empty spots with value 0 when the map is created*/
+
+    createMap(mapLayout);
+
     return 0;
 }
 
-void createMap(int map[MAX_X][MAX_Y])
+void createMap(int mapLayout[MAX_X][MAX_Y])
 {
     /*Asks which type of map the player wants, plains, mountains, etc*/
     int x,y;
+
+
     /*For a Plain map*/
-
-    srand(timeSeconds);
-
+    int number, result;
+    result = timeSeconds();
+    srand(result);
+    number = rand() % MAX_TERRAIN_HEIGHT_PLAINS_MAP; // 0 - 80  -> need to put 81;
+    for (x = 0; x < MAX_X; x++)
+    {
+        for (y = 0; y < number; y++)
+        {
+            mapLayout[x][y] = 1;  /*This will set everything under the surface height to be ground*/
+        }
+    }
     /*---End of plain map---*/
 }
 
@@ -53,5 +67,3 @@ int timeSeconds()
     seconds = t->tm_sec;
     return seconds;
 }
-
-
