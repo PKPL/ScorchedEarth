7780f9f78973e12538c9514a94038bb7e69760cb
anjagil <gil.anna20@gmail.com>
2014-03-23 21:18:30 +0100

    Highscore1

--- menu_highscore.c
+++ menu_highscore.c
@@ -1,3 +1,29 @@
 //---------------------------- Highscore ---------------------------------
 // Top ten scores of a game
 //------------------------------------------------------------------------
+
+
+#include <stdio.h>
+FILE *fp;
+
+int inChar;
+
+int main()
+{
+printf("Highscore\n\n");
+sort();
+
+fp=fopen("highscore.txt", "r+");
+
+
+     inChar = getc(fp);
+while (inChar != EOF)
+{
+putchar(inChar);
+inChar = getc(fp);
+}
+fclose(fp);
+return 0;
+}
+
+
