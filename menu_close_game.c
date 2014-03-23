8757eb0e120a81a2824b81d837ce9d93da2788ad
Jechbeat <jechbeatdev@gmail.com>
2014-03-23 14:02:36 +0100

    Close done

--- menu_close_game.c
+++ menu_close_game.c
@@ -1,3 +1,22 @@
 //---------------------------- Close game --------------------------------
 // Closing game and single play
 //------------------------------------------------------------------------
+
+#include <windows.h>
+#include <stdio.h>
+
+
+int close_game()
+{
+    char decision;
+    printf("Are you sure you want to quit the game? y/n");
+    scanf("%d",&decision);
+
+    if(decision == 'y') exit(EXIT_SUCCESS);
+    else
+    {
+    system("cls");
+    return(0);
+    }
+
+}
