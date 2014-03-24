3d8a538b3c3f468f632c0cdcd90ab860e5c40709
Nuno Valente <nuno.av@live.com.pt>
2014-03-24 19:00:15 +0000

    Finished the mountain terrain code, still have to write it into a function

--- maps_create.c
+++ maps_create.c
@@ -1,8 +1,10 @@
 /*
 PORTUGUESE TEAM
+
 NUNO VALENTE
 ROBERTO RIBEIRO
 DANIEL PINTO
+
 
 Changes done: Plain terrain generation done.
 
@@ -18,6 +20,10 @@ Changes done: Plain terrain generation done.
 #define MIN_X 0
 #define MIN_Y 0
 #define MAX_TERRAIN_HEIGHT_PLAINS_MAP 80
+#define MAX_TERRAIN_INITIAL_HEIGHT_MOUNTAINS_MAP 90
+#define MAX_DISTANCE_UNTIL_DEFORMATION_START 10
+#define MAX_WIDTH_DEFORMATION 31
+#define MAX_HEIGHT_DEFORMATION 95
 
 typedef struct
 {
@@ -26,6 +32,7 @@ typedef struct
 
 void createMap(int mapLayout[MAX_X][MAX_Y], int *counter);
 int timeSeconds(int *counter);
+void newRndSeed(int *counter);
 
 int main(void)
 {
@@ -41,12 +48,12 @@ int main(void)
 void createMap(int mapLayout[MAX_X][MAX_Y], int *counter)
 {
     /*Asks which type of map the player wants, plains, mountains, etc*/
-    int x,y, number, result;
+    int x,y, number;
 
     /*For a Plain map*/
-    result = timeSeconds(counter);
-    srand(result);
-    number = rand() % MAX_TERRAIN_HEIGHT_PLAINS_MAP; /* 0 - 80  -> need to put 81*/
+    newRndSeed(counter);
+    number = rand() % (MAX_TERRAIN_HEIGHT_PLAINS_MAP + 1); /* 0 - MAX HEIGHT  -> need to put MAX HEIGHT +1*/
+    /*This will loop for all the x coordinates*/
     for (x = 0; x < MAX_X; x++)
     {
         for (y = 0; y < number; y++)
@@ -58,7 +65,120 @@ void createMap(int mapLayout[MAX_X][MAX_Y], int *counter)
 
 
     /*For a mountain map*/
-    result = timeSeconds(counter);
+    int terrainDeformationStart, deformationWidth, deformationHeight, widthMedian, terrainUnitsBuilt, terrainUnitsUntilMedian, terrainUnitsToBuild, terrainUnitsAfterMedian, lastHeight;
+    terrainUnitsBuilt = 0;
+    lastHeight = 1; /*This line is just to avoid a warning that this var may be used uninitialized, however the variable will always be given a value with this program's code*/
+    newRndSeed(counter);
+    number = rand() % (MAX_TERRAIN_INITIAL_HEIGHT_MOUNTAINS_MAP + 1); /*defines the initial terrain height*/
+    /*Code for the deformation begins here, will have to be put into a function to repeat itself*/
+    newRndSeed(counter);
+    terrainDeformationStart = rand() % (MAX_DISTANCE_UNTIL_DEFORMATION_START + 1); /*defines the max distance until a deformation appears*/
+    /*This will loop for all the x coordinates at the same height, before the first deformation*/
+    for (x = 0; x < (terrainDeformationStart - 1); x++)
+    {
+        for (y = 0; y < number; y++)
+        {
+            mapLayout[x][y] = 1; /*This will create the surface line before the first deformation*/
+        }
+        terrainUnitsBuilt++;
+    }
+
+    do
+    {
+        newRndSeed(counter);
+        deformationWidth = rand() % (MAX_WIDTH_DEFORMATION + 1); /*defines the max width for the deformation. This will have to be an odd number for this implementation version*/
+    }
+    while (deformationWidth % 2 == 0 && deformationWidth < 5); /*Ensures the deformation has at least the width of 5 units, and the deformation width is an odd number*/
+
+    if ((terrainUnitsBuilt + deformationWidth) < MAX_X) /*If this is verified, we have enough space for another deformation*/
+    {
+        do
+        {
+            newRndSeed(counter);
+            deformationHeight = rand() %(MAX_HEIGHT_DEFORMATION + 1); /*defines the max height for the deformation.*/
+            /*For this implementation version, valleys may appear on a mountain, but valleys will never be forcefully created. This will change on a later revision*/
+        }
+        while (deformationHeight + number >= MAX_Y);
+
+        /*The following code prevents the deformation width to go outside map bounds, and if a new width is to be defined, it ensures it's an odd number*/
+        if (deformationWidth + number >= MAX_X && (MAX_X - number) % 2 != 0)
+        {
+            deformationWidth = MAX_X - number;
+        }
+        else if (deformationWidth + number >= MAX_X && (MAX_X - number - 1) % 2 != 0)
+        {
+            deformationWidth = MAX_X - number;
+        }
+        /*At this point we have found the width and height for the deformation*/
+        /*We need to find the width's median now*/
+        widthMedian = (deformationWidth/2) + 1; /*In this implementation version, the median of the width will have the highest height*/
+        terrainUnitsUntilMedian = deformationWidth - widthMedian;
+        terrainUnitsToBuild = terrainUnitsUntilMedian;
+        terrainUnitsAfterMedian = terrainUnitsUntilMedian;
+
+        for (x = terrainUnitsBuilt; terrainUnitsToBuild > 0; x++) /*This loop creates terrain deformation before reaching the mountain top. This can either create a mountain or a valley on a mountain.*/
+        {
+            do
+            {
+                newRndSeed(counter);
+                number = rand() % (MAX_HEIGHT_DEFORMATION + 1);
+            }
+            while (number <= deformationHeight);
+            for (y = 0; y < number; y++)
+            {
+                mapLayout[x][y] = 1;
+            }
+            terrainUnitsBuilt++;
+            terrainUnitsToBuild--;
+        }
+
+        /*Now we have reached the median width value, which will have the max height*/
+        x = terrainUnitsBuilt; /*This step is only for us to know that x has the value of the units built, but this is not a necessary line as that is already the value x holds. This is just a reminder*/
+        y = (deformationHeight - 1);
+        mapLayout[x][y] = 1;
+        terrainUnitsBuilt++;
+        /*Now the median value already has terrain*/
+        /*We need to assign terrain to the remaining values of the deformation width*/
+
+        terrainUnitsBuilt = terrainUnitsAfterMedian;
+        terrainUnitsToBuild = terrainUnitsAfterMedian;
+
+        for (x = terrainUnitsBuilt; terrainUnitsToBuild > 0; x++) /*This loop creates terrain deformation after reaching the mountain top. This can either create a mountain or a valley on a mountain.*/
+        {
+            do
+            {
+                newRndSeed(counter);
+                number = rand() % (MAX_HEIGHT_DEFORMATION + 1);
+            }
+            while (number <= deformationHeight);
+            for (y = 0; y < number; y++)
+            {
+                mapLayout[x][y] = 1;
+                lastHeight = y;
+            }
+            terrainUnitsBuilt++;
+            terrainUnitsToBuild--;
+        }
+        /*This concludes the building of a deformation*/
+    }
+    else /*Not enough blocks available for a deformation.*/
+    {
+        for (x = terrainUnitsBuilt; x < MAX_X; x++)
+        {
+            for (y = 0; y < lastHeight; y++)
+            {
+                mapLayout[x][y] = 1; /*Sets the rest of the map's surface to the last built height*/
+            }
+        }
+    }
+
+}
+
+void newRndSeed(int *counter)
+{
+    int seed;
+    seed = timeSeconds(counter);
+    srand(seed);
 }
 
 int timeSeconds(int *counter)
