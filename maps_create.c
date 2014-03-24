e27f60e237e26c19f3b9c2c661527dcc641f7fc5
Nuno Valente <nuno.av@live.com.pt>
2014-03-24 16:26:40 +0000

    Added counter to random seed
    Added a counter to force the seed to change each time srand is used,
    since srand uses the current second to generate the initial seed,
    therefore operations using srand during the same second would generate
    the same seed. That issue is fixed!

--- maps_create.c
+++ maps_create.c
@@ -24,28 +24,27 @@ typedef struct
     int x, y;
 } tCoordinates;
 
-void createMap(int mapLayout[MAX_X][MAX_Y]);
-int timeSeconds();
+void createMap(int mapLayout[MAX_X][MAX_Y], int *counter);
+int timeSeconds(int *counter);
 
 int main(void)
 {
+    int counterTimesUsedRandom = 0;
     int mapLayout[MAX_X][MAX_Y] = {{0}}; /*Matrix containing the map information initialized with value 0 everywhere*/
     /*Since the array is initialized with the value 0, which stands for empty in the map, we won't have to fill empty spots with value 0 when the map is created*/
 
-    createMap(mapLayout);
+    createMap(mapLayout, &counterTimesUsedRandom);
 
     return 0;
 }
 
-void createMap(int mapLayout[MAX_X][MAX_Y])
+void createMap(int mapLayout[MAX_X][MAX_Y], int *counter)
 {
     /*Asks which type of map the player wants, plains, mountains, etc*/
-    int x,y;
-
+    int x,y, number, result;
 
     /*For a Plain map*/
-    int number, result;
-    result = timeSeconds();
+    result = timeSeconds(counter);
     srand(result);
     number = rand() % MAX_TERRAIN_HEIGHT_PLAINS_MAP; /* 0 - 80  -> need to put 81*/
     for (x = 0; x < MAX_X; x++)
@@ -56,13 +55,19 @@ void createMap(int mapLayout[MAX_X][MAX_Y])
         }
     }
     /*---End of plain map---*/
+
+
+    /*For a mountain map*/
+    result = timeSeconds(counter);
 }
 
-int timeSeconds()
+int timeSeconds(int *counter)
 {
     int seconds;
     time_t now = time(NULL);
     struct tm *t = localtime(&now);
+    (*counter)++;
     seconds = t->tm_sec;
+    seconds += (*counter);
     return seconds;
 }
