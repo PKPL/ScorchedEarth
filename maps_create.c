63a7688b3a8cf63a96dc5871b02d8f8d4336b40c
Nuno Valente <nuno.av@live.com.pt>
2014-03-25 18:19:30 +0000

    Fixed first aspect mentioned by Pawel

--- maps_create.c
+++ maps_create.c
@@ -17,15 +17,15 @@ Bug hunting in progress.
 #include <windows.h>
 #include <time.h>
 
-#define MAX_X 100
-#define MAX_Y 100
+#define MAX_X 200
+#define MAX_Y 80
 #define MIN_X 0
 #define MIN_Y 0
-#define MAX_TERRAIN_HEIGHT_PLAINS_MAP 80
-#define MAX_TERRAIN_INITIAL_HEIGHT_MOUNTAINS_MAP 90
+#define MAX_TERRAIN_HEIGHT_PLAINS_MAP 60
+#define MAX_TERRAIN_INITIAL_HEIGHT_MOUNTAINS_MAP 70
 #define MAX_DISTANCE_UNTIL_DEFORMATION_START 10
 #define MAX_WIDTH_DEFORMATION 31
-#define MAX_HEIGHT_DEFORMATION 95
+#define MAX_HEIGHT_DEFORMATION 75
 
 typedef struct
 {
