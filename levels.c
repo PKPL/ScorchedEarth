c8ab1a0cc57926ded73f87ade6ee2ebae0212ef4
Jechbeat <jechbeatdev@gmail.com>
2014-03-25 21:02:11 +0100

    level struct

--- levels.c
+++ levels.c
@@ -8,7 +8,31 @@
 #include "levels_set.c"
 #include "levels_level.c"
 
+
 void test_levels() // function for agreagte testing
 {
-    printf("Levels test");
+    levels();
 }
+
+void levels()
+{
+    level_struct easy_level;
+    {
+        easy_level.level_wind = wind_type.no_wind;
+        easy_level.level_ai = ai_type.ai_easy;
+    }
+
+    level_struct medium_level;
+    {
+        easy_level.level_wind = wind_type.const_wind;
+        easy_level.level_ai = ai_type.ai_medium;
+    }
+
+    level_struct hard_level;
+    {
+        easy_level.level_wind = wind_type.variable_wind;
+        easy_level.level_ai = ai_type.ai_hard;
+    }
+
+}
+
--- levels_level.c
+++ levels_level.c
@@ -2,3 +2,22 @@
 // Stuctur resposible for levels setting
 //------------------------------------------------------------------------
 
+enum wind_type
+{
+  no_wind,
+  const_wind,
+  variable_wind
+};
+
+enum ai_type
+{
+  ai_easy,
+  ai_medium,
+  ai_hard
+};
+
+typedef struct level_struct
+{
+   wind_type level_wind;
+   ai_type level_ai;
+} level_struct;
