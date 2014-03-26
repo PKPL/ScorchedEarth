06ca6976dbb4bac50422172f22ad37ae7cef4f3c
anjagil <gil.anna20@gmail.com>
2014-03-27 00:40:16 +0100

    highscore 2

--- menu_highscore.c
+++ menu_highscore.c
@@ -1,12 +1,47 @@
 //---------------------------- Highscore ---------------------------------
 // Top ten scores of a game
 //------------------------------------------------------------------------
-
-
+#include <windows.h>
+#include <time.h>
 #include <stdio.h>
 FILE *fp;
+const int n= 10;
 
+  struct player
+ {
+   char nickname[20];
+   int points;
+ }player[11] ;
 int inChar;
+
+void bubblesort()
+{
+	int i, j, temp;
+	const int n=10;
+	for (i = 0; i<n-1; i++)
+        {
+		for (j=0; j<n-1-i; j++)
+		{
+			if (player[j].points > player[j+1].points)
+			{
+				temp =player[j+1];
+				player[j+1] = player[j];
+				player[j] = temp;
+			}
+		}
+        }
+}
+
+void add_score ()
+{
+
+}
+void check_top_ten()
+{
+    if (player[10].points> player[9].points)
+        sort();
+    else {}
+}
 
 void highscore()
 {
