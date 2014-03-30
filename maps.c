ac1af1f89597872e20d2e2dcfdc4e04d9a23bea9
danielmp <danny.mp@live.com.pt>
2014-03-30 20:03:42 +0100

    maps.c
    "putted // in some testing functions"

--- maps.c
+++ maps.c
@@ -10,5 +10,7 @@ void test_maps() // function for agreagate testing
     printf("Maps test\n");
 
     //test_maps_save();
-    test_maps_create();
+    //test_maps_load();
+    //test_maps_create();
+    //test_drawing_units ();
 }
