4e0cd9577a4abd8202238e71c9228ae4fa0f62c3
Nuno Valente <nuno.av@live.com.pt>
2014-03-31 15:45:52 +0100

    Commented map creation test
    Read the description on the commit below

--- maps.c
+++ maps.c
@@ -11,6 +11,6 @@ void test_maps() // function for agreagate testing
 
     //test_maps_save();
     //test_maps_load();
-    test_maps_create();
+    //test_maps_create();
     //test_drawing_units ();
 }
