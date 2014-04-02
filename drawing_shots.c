e760e294e1f41d7b1abc2f50d611bcc0b61bb409
Puzzlepack <roberto.tlmvl@gmail.com>
2014-04-02 16:41:07 +0100

    Missiles' trajectory CHECK!
    We finished the code responsible for drawing the missiles' trajectory!

--- drawing_shots.c
+++ drawing_shots.c
@@ -3,3 +3,41 @@
 //------------------------------------------------------------------------
 
 #include "drawing_shots.h"
+
+void test_drawing_shots ()
+{
+    int x, y;
+
+    int mapLayout[MAX_X][MAX_Y] = {{0}}; // just for testing
+    drawing_shots(mapLayout, m);
+}
+
+void drawing_shots (int mapLayout[MAX_X][MAX_Y], missile_data m)
+{
+    int i, j, k, x, y;
+
+    int vetor[VECTOR_LENGTH];
+
+    for (j = VECTOR_LENGTH, k = 0 ; j >= 0, k < VECTOR_LENGTH ; j-- , k++)
+    {
+        vetor[k] =  m->y_vector_coordinate[j]; // we need to change the the elements's order
+    }
+
+    for (i = 0; i < VECTOR_LENGTH; i++)
+    {
+        x = m->x_vector_coordinate[i];
+        y = vector[i];
+
+        if (mapLayout[x][y] != SKY) // if the missile hits something, it explodes
+        {
+            // EXPLOSION!
+            i = VECTOR_LENGTH; // it stops the for cycle
+        }
+        else // it draws the missile's trajectory
+        {
+            mapLayout[x][y]=PLAYER_SHOT; // We have no reference for the shooter, so it stays the player for now
+        }
+    }
+
+
+}
--- drawing_shots.h
+++ drawing_shots.h
@@ -6,3 +6,15 @@
 //------------------------------------------------------------------------
 
 #endif // __DRAWING_SHOTS_H__
+
+#include "shot_final_equation.h"
+
+#define MAX_X 100
+#define MAX_Y 80
+#define PLAYER_SHOT 50
+#define ENEMY_SHOT 60
+#define VECTOR_LENGTH 100
+#define SKY 0
+#define GROUND 1
+
+
