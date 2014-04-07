04554089eb841e58e4ef9584bdc83398e2008d9b
Nuno Valente <nuno.av@live.com.pt>
2014-04-07 20:48:01 +0100

    Preparing for implementation of drawing destruction

--- drawing.c
+++ drawing.c
@@ -11,5 +11,5 @@ void test_drawing(int mapLayout [MAX_X][MAX_Y]) // function for agreagte testing
 {
     printf("Drawing test\n");
     test_drawing_units(mapLayout);
-    test_drawing_destruction(mapLayout);
+    //test_drawing_destruction(mapLayout);
 }
--- drawing_destruction.h
+++ drawing_destruction.h
@@ -10,5 +10,6 @@
 void test_drawing_destruction(int mapLayout [MAX_X][MAX_Y]);
 void createExplosion(int mapLayout [MAX_X][MAX_Y]);
 void createDestruction(int mapLayout [MAX_X][MAX_Y]);
+int checkIfHit();
 
 #endif // __DRAWING_DESTRUCTION_H__
