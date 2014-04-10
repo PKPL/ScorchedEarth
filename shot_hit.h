1059ba5a0a4447605c212f7f45bbb7d829d1e3c1
PKPL <plempa@pk.edu.pl>
2014-04-10 18:46:32 +0200

    Fixed headers
    Fixed header in some files

--- shot_hit.h
+++ shot_hit.h
@@ -1,6 +1,11 @@
+#ifndef __SHOT_HIT_H__
+#define __SHOT_HIT_H__
+
 //On this file Team 1 is working: Federico Bollotta and Lorenzo Romanelli
 
 #include "matrix.h"
 #include "shot_final_equation.h"
 
 int checkHit(int, missile_data*);
+
+#endif // __SHOT_HIT_H__
