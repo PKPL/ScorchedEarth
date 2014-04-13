57c244065cf5adb01653bef312ad5bb9eb8287f5
anjagil <gil.anna20@gmail.com>
2014-04-13 23:45:13 +0200

    Variables change.

--- menu_highscore.c
+++ menu_highscore.c
@@ -4,9 +4,7 @@
 #include "menu_highscore.h"
 
 FILE *fp;
-const int n= 10;
-int inChar;
-char c;
+char c_open;
 
 	// bubble sorting
 
@@ -19,18 +17,18 @@ char c;
 	void bubblesort(void)
  	{
  fp=fopen("highscore.txt", "r+");
-		int i, j, temp;
-		const int n=10;
-		for (i = 0; i<n-1; i++)
+		int i_loop, j_loop, temp;
+
+		for (i_loop = 0; i_loop<9; i_loop++)
 	        {
 
-			for (j=0; j<n-1-i; j++)
+			for (j_loop=0; j_loop<9-i_loop; j_loop++)
 	            {
-	            if (players[j+1].points < players[j].points)
+	            if (players[j_loop+1].points < players[j_loop].points)
 				{
-					temp = players[j+1].points;
-					players[j+1].points =players[j].points;
-					players[j].points = temp;
+					temp = players[j_loop+1].points;
+					players[j_loop+1].points =players[j_loop].points;
+					players[j_loop].points = temp;
 				}
 			}
 	        }
@@ -38,10 +36,10 @@ char c;
 	}
 
 	void save_score (void)
-	{int counter;
-	    for (counter=0; counter<10; counter++)
-	    { fputs(players[counter].nickname,fp);
-	      fputs(players[counter].points,fp);
+	{int counter_loop;
+	    for (counter_loop=0; counter_loop<10; counter_loop++)
+	    { fputs(players[counter_loop].nickname,fp);
+	      fputs(players[counter_loop].points,fp);
 	    }
 
  	}
@@ -56,8 +54,8 @@ char c;
 
  	void highscore(void)
      {fp=fopen("highscore.txt", "r+");
-        while ((c=fgetc(fp))!=EOF)
-        printf("%c", c);
+        while ((c_open=fgetc(fp))!=EOF)
+        printf("%c", c_open);
 
     fclose(fp);
 }
