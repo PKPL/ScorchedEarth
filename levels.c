caa3c77c5f2565bc783e3f45bb5319f3143f4429
anjagil <gil.anna20@gmail.com>
2014-04-19 22:06:17 +0200

    level.c - simplier version
    As I have written in comment, it's a working stage  version, because we
    have problems with undestanding some parts of it.

--- levels.c
+++ levels.c
@@ -3,11 +3,18 @@
 //------------------------------------------------------------------------
 
 // Included libraries
-
+//
 // Included files
+
+// It was a hard nut for us to understand what kind of return is here. So we change this in working stage to something simplier
+
+// Ania Gil  and Nuno  Valente
+
+
+
 #include "levels.h"
 
-level_struct test_levels() // function for agreagate testing
+/*level_struct test_levels() // function for agreagate testing
 {
     //printf("Drawing test\n");
     level_struct selected_level;
@@ -35,4 +42,34 @@ level_struct test_levels() // function for agreagate testing
     while (selected_option < 1 || selected_option > 3 || control == 0);
 //    selected_level = level_set (selected_option); Problem with this function
     return selected_level;
+} */
+
+int test_levels() // function for agreagate testing
+{
+    //printf("Drawing test\n");
+    level_struct selected_level;
+    int selected_option, control;
+    int wrong_input = 0;
+    do
+    {
+        if (wrong_input == 1)
+        {
+            printf("The chosen option is not valid.\nNumber of desired option: ");
+            control = scanf("%d", &selected_option);
+            fflush(stdin);
+        }
+        else
+        {
+            printf("\nPick the desired level|\n");
+            printf("|______________________________________________|\n\n");
+            printf("[ 1 ] -> Easy\n[ 2 ] -> Medium\n[ 3 ] -> Hard\n");
+            printf("\nNumber of desired option: ");
+            control = scanf("%d", &selected_option);
+            fflush(stdin);
+            wrong_input = 1;
+        }
+    }
+    while (selected_option < 1 || selected_option > 3 || control == 0);
+//    selected_level = level_set (selected_option); Problem with this function
+    return selected_option;
 }
