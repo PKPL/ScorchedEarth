d2a63f34e533311fe3e54c87ffce44142281ee27
Nuno Valente <nuno.av@live.com.pt>
2014-04-19 20:38:27 +0100

    chaged function order

--- main.c
+++ main.c
@@ -28,9 +28,9 @@ int main()
     test_ai();
     //selected_level = test_levels();
     printf("\nThere is an occasional crash during the map testing,\nbut only if you are generating a mountain map.\nIf this happens just run the program again.\nWe couldn't find the source of this yet.\n\n");
+    test_menu();
     test_maps(map_layout);
     test_drawing(map_layout);
-    test_menu();
     test_unit();
     testShot();
     printf("\n-------------------------------------------------\n");
