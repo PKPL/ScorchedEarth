b093cf041897acf107b9659247b3e6550865a493
Jechbeat <jechbeatdev@gmail.com>
2014-05-07 18:05:25 +0200

    Just added some debug tools to shoot draw

--- drawing_shots.c
+++ drawing_shots.c
@@ -29,16 +29,19 @@ void drawing_shots (int map_layout[MAX_X][MAX_Y], missile_data m)
     for (i = 0; i < VECTOR_LENGTH; i++)
     {
         x = m.x_vector_coordinate[i];
+        //printf("x = %d", x);
         y = vetor[i];
-
+        //printf("  y = %d", y);
         if (map_layout[x][y] != SKY) // if the missile hits something, it explodes
         {
             // EXPLOSION!
             i = VECTOR_LENGTH; // it stops the for cycle
+
         }
         else // it draws the missile's trajectory
         {
             map_layout[x][y]=PLAYER_SHOT; // We have no reference for the shooter, so it stays the player for now
+
         }
     }
 
